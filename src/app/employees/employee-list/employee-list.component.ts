e9d53d25b44d8589bbe601180b0796eb71c2e8dd
Jouhaina <jouhainahmida@gmail.com>
2020-07-30 02:24:29 +0100

    Read

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
+import { Employee } from './../../shared/employee.model';
+import { EmployeeService } from 'src/app/shared/employee.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  list: Employee[];
+  constructor(private service: EmployeeService) { }
 
   ngOnInit(): void {
+    this.service.getEmployees().subscribe((actionArray) => {
+      this.list = actionArray.map((item) => {
+        return {
+          id: item.payload.doc.id,
+          ...(item.payload.doc.data() as Employee),
+        };
+      });
+    });
+  }
   }
 
-}
